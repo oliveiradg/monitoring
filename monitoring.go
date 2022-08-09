8f02fc99ebcd36f9fd5a5386e38a3428eb839ea0
Joao Oliveira <j.oliveiradg@gmail.com>
2022-08-09 10:11:28 -0300

    feat: criando a função, imprimeLogs

--- monitoring.go
+++ monitoring.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -25,6 +27,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exinindo Logs...")
+			imprimelogs()
 		case 0:
 			fmt.Println("saindo do programa...")
 			os.Exit(0)
@@ -141,6 +144,14 @@ func registraLog(site string, status bool) {
 		fmt.Println("Ocorreu um erro: ", err)
 	}
 
-	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + fmt.Sprint(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 	arquivo.Close()
+}
+
+func imprimelogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro: ", err)
+	}
+	fmt.Println(string(arquivo))
 }
